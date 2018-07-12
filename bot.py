54ca36a8975c1874d6454ac8d15daf54cf491328
JCarbee98 <40305948+JCarbee98@users.noreply.github.com>
2018-07-11 23:08:01 -0400

    Update bot.py
    Changes as of 7.11.2018

--- bot.py
+++ bot.py
@@ -8,42 +8,38 @@
 import tweepy
 import time
 from secrets import *
-from database import adduser,printusr
 
-# create an OAuthHandler instance
+#create an OAuthHandler instance
 # twitter requires all requests to use OAuth for authentication
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 
 api = tweepy.API(auth)
 
-public_tweets = api.home_timeline()
+#public_tweets = api.home_timeline()
 
+#for tweet in public_tweets:
+#	print(tweet.text)
 
-def limit_handled(cursor):  # code to wait 15 min when rate limited before trying again
-    while True:
-        try:
-            yield cursor.next()
-        except tweepy.RateLimitError:
-            time.sleep(15 * 60)
-
-
-for tweet in public_tweets:
-    print(tweet.text)
 
 print("hello!\n\n")
-twt = api.search(q="Hello World!")
-t = ['Hello world!', 'Hello World!', 'Hello World!!!', 'hello world!', 'Hello, World!']
+#twt = api.search(q="Hello World!")
 
-for s in twt:
-    for i in t:
-        if i == s.text:
-            sn = s.user.screen_name
-            m = "@%s Hello!" % (sn)
-            s = api.update_status(m, s.id)
+#api.send_direct_message(user="@murillians_tho", text="hello there")
+test = "@"
+counter = 0
+StayLoop = 0
+while StayLoop == 0:
+	userChoice = 0
+	print("MENU CHOICE 1:  TWEET")
+	print("MENU CHOICE 2:  EXIT")
+	userChoice = input("NUM:")
+	if userChoice == 1:
+		message = raw_input("Type the message: ")
+		s = api.update_status(message)
+	if userChoice == 2:
+		StayLoop = 1
 
-for follower in tweepy.Cursor(api.followers).items():
-    follower.follow()
+print("\ntwitter bot updated 7.11.2018")
 
-adduser(str(api.me().name))#adds own usrname to database
-printusr(api.me().name)#prints database matches to username
+		
