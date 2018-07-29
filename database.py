ad4e9517fe041f259a5505887159112fcf7c3b26
Murillians <eem15@my.fsu.edu>
2018-07-29 19:41:04 -0400

    Database Commit 2
    fixed to make it function properly as a function

--- database.py
+++ database.py
@@ -20,8 +20,13 @@ def addrpost (title,link,category):
     maindb.execute("insert into rposts values (?,?,?)",(title,link,category))
     db.commit()
 def getrpost(category):
-    maindb.execute("select * from rposts where category=:category LIMIT 1",{"category":category})
-    return (maindb.fetchall())
+    maindb.execute("select * from rposts where category=:category LIMIT 1", {"category": category})
+    rlist=[]
+    rows=maindb.fetchall()
+    for row in rows:
+        title,link,category=row
+        rlist.extend((title,link,category))
+    return rlist
 def printusr(usrname):
     maindb.execute("select * from twitacct where atname=:uname", {"uname": usrname})
     print(maindb.fetchone())
@@ -30,4 +35,6 @@ addtuser("memes",123)
 addrpost("TIL C fucking sucks","https://www.stilldrinking.org/programming-sucks",'programming')
 reddit=getrpost('programming')
 print(reddit)
-db.close()
+print(reddit[0])
+print(reddit[1])
+print(reddit[2])
