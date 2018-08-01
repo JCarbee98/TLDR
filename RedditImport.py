64fc9df117dcbd06a0f0c239dd4cb5f3f8fde3d0
Brian Pianka <brian.pianka1@gmail.com>
2018-08-01 14:10:40 -0400

    Modified grab_posts function to take proper arguments Added grab_random function to pull the top post from a random subreddit

--- RedditImport.py
+++ RedditImport.py
@@ -6,14 +6,8 @@ PRAW is a Reddit interface object that allows the scraping of posts from subredd
 
 To use grab_posts in it's current state, pass a list of subreddits, a list of numerical arguments for which posts to
 grab, and an optional amount of posts to grab (defaults to one post). The function will scrape the desired posts from
-Reddit and return a list of lists (the categories of posts) containing lists (the actual posts) which hold the post's
+Reddit and return a list containing lists (the actual posts) which hold the post's
 title, score, and url. You can then parse this returned structure to display the information or send it over twitter.
-
-0 = top
-1 = controversial
-2 = hot
-3 = gilded
-4 = top post of the day from a random subreddit
 
 '''
 
@@ -23,65 +17,29 @@ red = praw.Reddit(user_agent='windows:example.twitterredditbot:v.1 (by students)
                   client_secret='1DePUPByrds4R7lAk6xv1NyE1ww')
 
 
-def grab_posts(subs, cats, numposts=1): #default value, can be made larger for public posting
+def grab_posts(subs, numposts=1): #default value, can be made larger for grabbing more posts
     info = []   #list to hold lists of info
 
     for sub in subs:    #for all the subs we need to go through
-        for num in cats:
-            category = []  # container for posts from a certain category
+        for post in red.subreddit(sub).top('day', limit=numposts):
+            pst = []  # list to hold singular post info
 
-            if num == 0:    #top
-                for post in red.subreddit(sub).top('day', limit=numposts):
-                    pst = []  # list to hold singular post info
+            pst.append(post.title)  #collect data
+            pst.append(post.score)
+            pst.append(post.url)
 
-                    pst.append(post.title)  #collect data
-                    pst.append(post.score)
-                    pst.append(post.url)
-
-                    category.append(pst)        #append to category holder
-                info.append(category)
-            elif num == 1:
-                for post in red.subreddit(sub).controversial('day', limit=numposts):
-                    pst = []  # list to hold singular post info
-
-                    pst.append(post.title)
-                    pst.append(post.score)
-                    pst.append(post.url)
-
-                    category.append(pst)
-                info.append(category)
-            elif num == 2:
-                for post in red.subreddit(sub).hot('day', limit=numposts):
-                    pst = []  # list to hold singular post info
-
-                    pst.append(post.title)
-                    pst.append(post.score)
-                    pst.append(post.url)
-
-                    category.append(pst)
-                info.append(category)
-            elif num == 3:
-                for post in red.subreddit(sub).gilded('day', limit=numposts):
-                    pst = []  # list to hold singular post info
-
-                    pst.append(post.title)
-                    pst.append(post.score)
-                    pst.append(post.url)
-
-                    category.append(pst)
-                info.append(category)
-            elif num == 4:
-                for post in red.random_subreddit().top('day', limit=numposts):
-                    pst = [] # list to hold singular post info
-
-                    pst.append(post.title)
-                    pst.append(post.score)
-                    pst.append(post.url)
-
-                    category.append(pst)
-                info.append(category)
+            info.append(pst)
     return info                     #return it
 
+def grab_random():	#grabs the top post over a day from a random subreddit and returns it
+	for post in red.random_subreddit().top('day', limit=1):
+		pst = []
+
+		pst.append(post.title)
+		pst.append(post.score)
+		pst.append(post.url)
+
+		return pst
 
 
 '''
@@ -106,18 +64,11 @@ while True:
         break
     else:
         subreddits.append(choice)
-print("Please enter the types of posts you want: \n\n0 = top\n1 = controversial\n2 = hot\n3 = gilded\n4 = top from a random sub\n\n")
-while True:
-    choice = input()
-    if choice == "done":
-        break
-    else:
-        post_types.append(int(choice))
 
 number = int(input("Please enter the number of posts you want: "))
 
-stuff = grab_posts(subreddits, post_types, number)
+stuff = grab_posts(subreddits, number)
+random = grab_random()
 
-print(subreddits)
-print(post_types)
 print(stuff)
+print(random)
